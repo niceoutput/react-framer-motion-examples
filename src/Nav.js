5f6f7265d3aebb7519d54a71e0408e65025c626a
mario <mario@uniplaces.com>
2020-07-13 16:26:22 +0100

    add staggering animations to the menu links

--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { motion } from "framer-motion";
 
 const variants = {
@@ -7,28 +7,42 @@ const variants = {
   closed: { opacity: 0, x: "-100%" },
 };
 
+const liVariants = {
+  open: { y: 0, opacity: 1 },
+  closed: { y: -20, opacity: 0 },
+};
+
+const ulVariants = {
+  open: {
+    scale: 1.05,
+    transition: {
+      staggerChildren: 0.3,
+      delayChildren: 0.2,
+      staggerDirection: 1,
+      when: "afterChildren",
+    },
+  },
+  closed: {},
+};
+
+const links = ["one", "two", "three", "four"];
+
 const Nav = ({ isNavOpen, setIsNavOpen }) => {
   return (
     <MenuNav
       variants={variants}
       initial='closed'
       animate={isNavOpen ? "open" : "closed"}
+      transition={{ damping: 300 }}
     >
       <button onClick={() => setIsNavOpen(false)}>Close</button>
-      <ul>
-        <li>
-          <a href='#'>One</a>
-        </li>
-        <li>
-          <a href='#'>Two</a>
-        </li>
-        <li>
-          <a href='#'>Three</a>
-        </li>
-        <li>
-          <a href='#'>Four</a>
-        </li>
-      </ul>
+      <motion.ul variants={ulVariants}>
+        {links.map((link) => (
+          <motion.li variants={liVariants} key={link}>
+            <a href='#'>{link}</a>
+          </motion.li>
+        ))}
+      </motion.ul>
     </MenuNav>
   );
 };
@@ -48,13 +62,20 @@ const MenuNav = styled(motion.nav)`
     margin: 0;
 
     li {
-      margin: 0;
+      margin: 0 0 1rem;
       padding: 0;
       font-size: 2rem;
       display: block;
 
       a {
         color: white;
+        text-decoration: none;
+        border-bottom: 2px transparent solid;
+        transition: 0.3s ease border;
+
+        &:hover {
+          border-bottom: 2px solid var(--blue);
+        }
       }
     }
   }
