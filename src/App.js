4feb43fd791b6508c0c2266dc17b41f54852ef5b
mario <mario@uniplaces.com>
2020-07-14 15:21:48 +0100

    add drag and drop functionality to the cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  AnimatePresence,
+} from "framer-motion";
 import { Card, CardGrid, Container, Header } from "./Elements";
 import "./App.css";
 import Menu from "./Menu";
@@ -14,6 +20,10 @@ function App() {
   const [value, setValue] = useState(0);
   const [isToggled, setToggle] = useState(false);
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isCardActive, setIsCardActive] = useState(true);
+
+  const x = useMotionValue(0);
+  const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]);
 
   return (
     <div>
@@ -50,14 +60,49 @@ function App() {
             <h3>Some card</h3>
             <img src={purp} alt='' />
           </Card>
-          <Card style={{ background: "var(--blue)" }}>
+          <Card
+            drag
+            dragConstraints={{
+              top: -100,
+              left: -100,
+              bottom: 100,
+              right: 100,
+            }}
+            style={{ background: "var(--blue)" }}
+          >
             <h3>Some card</h3>
             <img src={blue} alt='' />
           </Card>
-          <Card style={{ background: "var(--black)" }}>
-            <h3>Some card</h3>
-            <img src={black} alt='' />
-          </Card>
+          <AnimatePresence>
+            {isCardActive && (
+              <motion.div
+                exit={{ height: 0, overflow: "hidden", opacity: 0 }}
+                transition={{
+                  opacity: {
+                    duration: 0,
+                  },
+                }}
+              >
+                <Card
+                  drag='x'
+                  dragConstraints={{ left: 0, right: 0 }}
+                  onDragEnd={(event, info) => {
+                    if (Math.abs(info.point.x) > 200) {
+                      setIsCardActive(false);
+                    }
+                  }}
+                  style={{
+                    x,
+                    opacity,
+                    background: "var(--black)",
+                  }}
+                >
+                  <h3>Some card</h3>
+                  <img src={black} alt='' />
+                </Card>
+              </motion.div>
+            )}
+          </AnimatePresence>
           <Card style={{ background: "var(--green)" }}>
             <h3>Some card</h3>
             <img src={green} alt='' />
